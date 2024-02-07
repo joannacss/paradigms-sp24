f13d52f3b5f99d461ac97c6e870a72a3933d95ed
Replit user <>
2024-02-07 23:12:35 +0000

    Added code demo-ed in class

--- JavaScript/live_dfs.js
+++ JavaScript/live_dfs.js
@@ -3,39 +3,41 @@
  * @param graph A directed graph represented as an adjency matrix.
  * @param root The initial node to start the traversal
  */
-function dfs(graph, root){
-    // initializes relevant data structures
-    	// a stack initialized with root node
-    	// an output to be returned
-    	// a set that tracks already visited node
-    
-    // iterate over each node to be visited
-    
-        // pop the node from the toVisit stack
-        
-        // check if node is visited
-        
-            // add node to the output
-            // mark node as visited       
-            // iterate over the children (in reverse order)
-            
-        
-    
-	// return the traversed nodes
-    
+function dfs(graph, root) {
+  // initializes relevant data structures
+  let toVisit = [root]; // a stack initialized with root node
+  let output = []; // an output to be returned
+  let visited = new Set(); // a set that tracks already visited node
+
+  // iterate over each node to be visited
+  while (toVisit.length > 0) {
+    // pop the node from the toVisit stack
+    let node = toVisit.pop();
+    // check if node is visited
+    if (!visited.has(node)) {
+      // add node to the output
+      output.push(node);
+      // mark node as visited
+      visited.add(node);
+      // iterate over the children (in reverse order)
+      let children = graph[node];
+      for (let i = children.length - 1; i >= 0; i--) {
+        toVisit.push(children[i]);
+      }
+    }
+  }
+  // return the traversed nodes
+  return output;
 }
 
-
-
 // test case 1
-graph = {"+": ["*",3], "*":[2,7], 2:[],7:[],3:[]};
-root = "+" ;
+graph = { "+": ["*", 3], "*": [2, 7], 2: [], 7: [], 3: [] };
+root = "+";
 output = dfs(graph, root);
 console.log(output);
 
-
 // test case 2
-graph = {0: [1,3], 1:[2,3], 2:[3,1], 3:[0,1]};
-root = 0 ;
+graph = { 0: [1, 3], 1: [2, 3], 2: [3, 1], 3: [0, 1] };
+root = 0;
 output = dfs(graph, root);
 console.log(output);

d30164c76cc941ded451126fed87e7b214ba9688
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-02-19 11:37:34 -0500

    Added code to be shown today

--- FrontEnd/example_cse/demo.js
+++ FrontEnd/example_cse/demo.js
@@ -1,7 +1,22 @@
-// Q1: Use DOM to change the H1 element to “Computer Science and Engineering Faculty”
+// Q1
+let h1 = document.getElementsByTagName("h1");
+h1[0].innerText = "Computer Science and Engineering Faculty";
 
 
-// Q2: Change everyone’s names to be Prof. Last Name
+// Q2
+let h2 = document.getElementsByTagName('h2');
+
+for(let i = 0 ; i < h2.length ; i++){
+	let el = h2[i];
+	let name = el.children[0].innerText;
+	let parts = name.split(" ");
+	let lastName = parts[parts.length - 1];
+	el.innerText = `Prof. ${lastName}`
+}
 
 
-// Q3: Change Dr. Cleland-Huang’s link to her lab website (https://sarec.nd.edu)
+// Q3
+
+let anchor = document.getElementById("post-1033-link");
+anchor.href = "https://sarec.nd.edu";
+console.log(anchor);

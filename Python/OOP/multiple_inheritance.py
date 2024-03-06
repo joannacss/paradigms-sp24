1b5dacab3fc68d91037fcdcc6dcc80edf8f386d0
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-03-05 21:28:42 -0500

    Added easter egg on the example

--- Python/OOP/multiple_inheritance.py
+++ Python/OOP/multiple_inheritance.py
@@ -1,5 +1,5 @@
 from animal import Cow
-
+import os
 
 class Vocalizer:
     def speak(self):
@@ -8,10 +8,21 @@ class Vocalizer:
 
 class EvilCow(Vocalizer, Cow):
     def sound(self):
+        try:
+            from playsound import playsound
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            playsound(os.path.join(current_dir, 'sound', 'ringtones-vaca-loca.mp3'))
+        except:
+            print('You likely forgot to pip install playsound PyObjC')
+
+
         return "moo ha ha mwahaha"
+
 
 
 if __name__ == '__main__':
     c = EvilCow('Bessie')
+    print("What does the evil cow says?")
+    print(c.sound())
     print(c.speak())  # what the output should be in here?
     print(EvilCow.mro())

c9ee5c07a31b1212bf099dbcd614c9758747fa0f
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-03-18 14:22:23 -0400

    Added initial code for website

--- Django/website/polls/views.py
+++ Django/website/polls/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from .models import Question, Choice
 
 
 def index(request):
@@ -7,3 +11,35 @@ def index(request):
         "semester": "Spring 2024",
     }
     return render(request, "polls/index.html", context)
+
+
+class IndexView(ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))

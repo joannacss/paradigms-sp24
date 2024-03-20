f94c68f77831ae4dd14b19d99490634b784f1df0
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-03-20 12:23:06 -0400

    Added allowed hosts configuration

--- Django/live_website/live_website/settings.py
+++ Django/live_website/live_website/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-va@qrbj&xm!_$sxt)q#6obz%q5wkx1sj*e&u211t!yqdu)9(kz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '129.74.152.125',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition

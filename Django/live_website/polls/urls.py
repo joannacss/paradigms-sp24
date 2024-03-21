62b308b8b028bac069b6aedf0619ea89e568dae1
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-03-20 23:35:42 -0400

    Added code demo-ed in class

--- Django/live_website/polls/urls.py
+++ Django/live_website/polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import IndexView
-
+from .views import QuestionView
+from .views import vote
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:pk>/", QuestionView.as_view(), name="detail"),
+    path("<int:pk>/vote/", vote, name="vote")
 ]
--- Django/live_website/polls/views.py
+++ Django/live_website/polls/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Question, Choice
+
 
 # Create your views here.
 def index(request):
@@ -19,7 +21,28 @@ class IndexView(ListView):
         """TODO: Return the last five published questions."""
         # Implementation below returns *all* questions
         return Question.objects.all()
+        # return Question.objects.order_by('-pub_date')[:5]
 
 
 
+class QuestionView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect("/polls")
 

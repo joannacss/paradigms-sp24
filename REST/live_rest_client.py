f615fccb0f1857d0191271eeececbeb15e70d593
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-03-27 14:56:39 -0400

    Added rest client example demo in class

--- REST/live_rest_client.py
+++ REST/live_rest_client.py
@@ -1,17 +1,27 @@
 import requests
 import csv
 
-HOST = "http://jcssdev.pythonanywhere.com/"
+HOST = "http://jcssdev.pythonanywhere.com"
 
 
 def main():
 
-	# makes HTTP GET request
+	with open("bugs.csv", "w") as f:
+		csv_writer = csv.writer(f, delimiter=",", quotechar='"')
+		next_page = f"{HOST}/bugs/"
+		while next_page:	
+			# makes HTTP GET request
+			response = requests.get(next_page)
 
-	# parses the response (.content vs .text vs .json())
+			# parses the response (.content vs .text vs .json())
+			json_response = response.json()
 
-	# iterate over each bug to write it into a file
+			# iterate over each bug to write it into a file
+			for bug in json_response["results"]:
+				csv_writer.writerow( [bug["id"], bug["status"], bug["summary"], bug["package"]])
 
+			next_page = json_response["next"]
+	
 	
 
 

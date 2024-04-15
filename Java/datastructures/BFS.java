42aebd0cd4c0cf78bd3e4be84fb56f99413c4fda
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-04-15 15:18:58 -0400

    Added data structures demo

--- Java/datastructures/BFS.java
+++ Java/datastructures/BFS.java
@@ -14,17 +14,34 @@ import java.util.Set;
 public class BFS {
     public List<String> traverse(String root, Map<String,List<String>> graph){
         // queue with nodes to be visited
-        
+        Queue<String> toVisit = new LinkedList<>(); 
+        toVisit.add(root);
         // list of visited nodes
-        
+        List<String> output = new ArrayList<>();
         // set of visited nodes, to avoid getting stuck
+        Set<String> visited = new HashSet<>();
+
         
 
         // keep visiting while there are nodes in the queue
-        
+        while(!toVisit.isEmpty()){
+            String node = toVisit.poll();
 
+            if(!visited.contains(node)){
+                // mark node as visited
+                visited.add(node);
+                output.add(node);
+                // iterate over node's children
+                List<String> children = graph.get(node);
+                for(String c: children){
+                    toVisit.add(c);
+                }
+            }
+
+        }
 
         // returns the visited nodes, in order
+        return output;
         
     }
 
